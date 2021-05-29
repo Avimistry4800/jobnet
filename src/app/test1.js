113033b1e4778e3f2ef251726d43ba51a3be3ca0
Noman62 <noman.ice.nstu@gmail.com>
2021-05-29 11:33:32 -0700

    initial

---
+++ src/app/test1.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const test1 = () => {
+    return (
+        <div>
+            <h1>test1</h1>
+            <p>hame</p>
+        </div>
+    );
+};
+
+export default test1;
