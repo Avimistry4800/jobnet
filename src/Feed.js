b547d4a3a20685dc2bdfa505c5246a73b23481c5
Noman62 <noman.ice.nstu@gmail.com>
2021-05-29 13:17:39 -0700

    input section done

--- src/Feed.js
+++ src/Feed.js
@@ -2,6 +2,12 @@ import React, { useState, useEffect } from 'react';
 import { db } from './Firebase';
 import './Feed.css';
 import CreateIcon from "@material-ui/icons/Create";
+import ImageIcon from "@material-ui/icons/Image";
+import SubscriptionsIcon from "@material-ui/icons/Subscriptions";
+import EventNoteIcon from "@material-ui/icons/EventNote";
+import CalendarViewDayIcon from "@material-ui/icons/CalendarViewDay";
+import InputOption from './InputOption';
+
 
 const Feed = () => {
 const [input, setInput] =useState('')
@@ -42,6 +48,12 @@ const sendPost = (e) =>{
                         <input type="text" />
                         <button type="submit">Send</button>
                     </form>
+                </div>
+                <div className="feed_inputOptions">
+                    <InputOption Icon={ImageIcon} title='photo' color="#7085F9" />
+                    <InputOption Icon={SubscriptionsIcon} title='Video' color="#E7A33E" />
+                    <InputOption Icon={EventNoteIcon} title='Event' color="#C0CBCD" />
+                    <InputOption Icon={CalendarViewDayIcon} title='Write article' color="#7FC15E" />
                 </div>
             </div>
         </div>
