c695f2b324a54996f26c4ec9661f6500ff3d513d
Md Ibal Hossain <iqbal.h1992@gmail.com>
2021-05-30 18:52:34 +0600

    login2

--- src/Login.js
+++ src/Login.js
@@ -1,23 +1,45 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './Login.css'
-const Login = () => {
-    const loginToApp = () => {
 
+const Login = () => {
+    const [email, setEmail] = useState("")
+    const [password, setPassword] = useState("")
+    const [name, setName] = useState("")
+    const loginToApp = (e) => {
+        e.preventDefault();
     }
-    const register = () =>{
+    const register = () => {
 
     }
     return (
-        <div className= "login">
+        <div className="login">
             <form>
-                <input type="text" placeholder="Full name- Required"/>
-                <input type="text" placeholder="Profile url-optional"/>
-                <input type="email" placeholder="yourmail@mail.com"/>
-                <input type="password" placeholder="password"/>
+                <input 
+                type="text" 
+                placeholder="Full name- Required"
+                value = {name}
+                onchange={e => setName(e.target.value)}
+                />
+                <input 
+                type="text" 
+                placeholder="Profile url-optional" 
+                value = {profilePic}
+                />
+                <input 
+                value={email} 
+                onchange={e => setEmail(e.target.value)} 
+                type="email" 
+                placeholder="yourmail@mail.com" />
+                <input 
+                type="password" 
+                placeholder="password"
+                value = {password}
+                onChange={ e => setPassword(e.target.value) }
+                />
                 <button type="submit" onClick={loginToApp}> Sign In </button>
             </form>
-            <p> Not a member?
-                <span className = "login__register" onClick= {register}>Register Now</span>
+            <p> Not a member?{" "}
+                <span className="login__register" onClick={register}>Register Now</span>
             </p>
         </div>
     );
